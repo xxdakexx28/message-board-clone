bb12b9fe35d3173f3d88614af2dc2b51b025c9fe
jlbnven <blanconjl@gmail.com>
2024-11-16 12:29:58 -0400

    class create

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
